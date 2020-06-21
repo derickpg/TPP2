f694db17e54312ebcabb78d1085df8c5e82bc4cb
derickpg <garcezderick@gmail.com>
2020-06-21 14:16:13 -0300

    Nivel chegando, e o resto tambem

--- t4c.c
+++ t4c.c
@@ -3,25 +3,27 @@
 
 main(int argc, char **argv)
 {
-    printf("=-=-= COMECOU O ALG DIVISAO E CONQUISTA =-=-=");
     int id_proc, qtd_proc;
     int id_pai, id_filho_esq, id_filho_dir;
-    int nivel, i = 0;
-    int tam_vet = 100, nums_vet = 10000;
+    int nivel[1];
+    int i = 0;
+    int tam_vet = 100, nums_vet = 10000; // o que eh nums_vet ?
     int tam_esq, tam_dir, tam_novo_vetor;
     int vetor[tam_vet];
     // TAGS
     int t_vetor = 50;
+    int t_morte = 99;
+    int t_nivel = 1;
 
     MPI_Status status; /* Status de retorno */
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &id_proc);
     MPI_Comm_size(MPI_COMM_WORLD, &qtd_proc);
 
-    printf("Greetings from process %d!", id_proc);
+    printf("\nSou o Processo => %d!", id_proc);
     if (id_proc == 0) /* Raiz */ {
 
-        nivel = 1; //Raiz inicializa o nivel como um, e passa para os filhos
+        nivel[0] = 1; //Raiz inicializa o nivel como um, e passa para os filhos
         for (i = 0; i < tam_vet; i++)
             vetor[i] = rand() % nums_vet + 1;
 
@@ -41,18 +43,34 @@ main(int argc, char **argv)
         }
         id_filho_esq = id_proc * 2 + 1;
         id_filho_dir = id_proc * 2 + 2;
+        nivel[0] = 2;
+        MPI_Send(nivel, tam_vet, MPI_INT, id_filho_esq, t_nivel, MPI_COMM_WORLD);
+        MPI_Send(nivel, tam_vet, MPI_INT, id_filho_dir, t_nivel, MPI_COMM_WORLD);
+
         MPI_Send(vet_esq, (tam_vet/2), MPI_INT, id_filho_esq, t_vetor, MPI_COMM_WORLD);
         MPI_Send(vet_dir, (tam_vet/2), MPI_INT, id_filho_dir, t_vetor, MPI_COMM_WORLD);
 
+
+        // MSG da MORTE
+        for(i = 0; i < qtd_proc; i ++){
+            MPI_Send(0, 0, MPI_INT, i, t_morte, MPI_COMM_WORLD);
+        }
+
     } else /* Nodos abaixo da raiz, varios niveis */ {
         id_pai = (id_proc - 1) / 2;
-        // nivel = nivel + 1;
-        // tam_novo_vetor = tam_vet / (pow(2, (nivel - 1)));
+        while(1){
+            MPI_Recv(vetor, 500, MPI_INT, id_pai, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            if(status.MPI_TAG == t_vetor){
+                printf("\n Chegou !!! %d", id_proc);
 
-        MPI_Recv(vetor, 500, MPI_INT, id_pai, t_vetor, MPI_COMM_WORLD, &status);
-        
-        printf("\n Chegou !!! %d", id_proc);
+            }else if(status.MPI_TAG == t_morte){
+                printf("\n MORRI !!! %d", id_proc);
+                MPI_Finalize();
+                return 0;
+            }else if(status.MPI_TAG == t_nivel){
+                printf("\n NIVEL CHEGOU !!! %d", id_proc);
+            }
+        }
     }
     MPI_Finalize();
-    printf("=-=-= ENCERROU O ALG DIVISAO E CONQUISTA =-=-=");
 }
